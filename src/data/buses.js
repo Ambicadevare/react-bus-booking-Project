1be9a133e953b60ce4e2fa1ecd932254924cc48c
ambicadevare <ambicadevare@gmail.com>
2026-02-17 19:53:36 +0530

    added multiple routes

--- src/data/buses.js
+++ src/data/buses.js
@@ -1,5 +1,12 @@
 export const buses = [
     { id: 1, name: "Orange Travels", from: "Hyderabad", to: "Bangalore", time: "10:30 PM" },
-    { id: 2, name: "VRL Travels", from: "Hyderabad", to: "Chennai", time: "9:00 PM" },
-    { id: 3, name: "SRS Travels", from: "Bangalore", to: "Hyderabad", time: "11:15 PM" }
+    { id: 2, name: "VRL Travels", from: "Hyderabad", to: "Chennai", time: "09:00 PM" },
+    { id: 3, name: "SRS Travels", from: "Bangalore", to: "Hyderabad", time: "11:15 PM" },
+    { id: 4, name: "Kaveri Travels", from: "Hyderabad", to: "Mumbai", time: "08:45 PM" },
+    { id: 5, name: "Morning Star", from: "Chennai", to: "Bangalore", time: "07:30 AM" },
+    { id: 6, name: "GreenLine", from: "Bangalore", to: "Chennai", time: "06:00 AM" },
+    { id: 7, name: "TSRTC", from: "Hyderabad", to: "Vijayawada", time: "05:30 PM" },
+    { id: 8, name: "APSRTC", from: "Vijayawada", to: "Hyderabad", time: "04:45 PM" },
+    { id: 9, name: "KPN Travels", from: "Chennai", to: "Hyderabad", time: "09:15 PM" },
+    { id: 10, name: "Orange Travels", from: "Bangalure", to: "Mumbai", time: "10:00 PM" }
 ];
